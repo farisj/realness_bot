dbf3368a18974c95b1fa39eb6fb3825fe79954b1
farisj <jakefaris@outlook.com>
2016-03-05 16:53:31 -0500

    Initial Commit

---
+++ bot.rb
@@ -0,0 +1,68 @@
+require 'twitter'
+require 'open-uri'
+
+SUBJECTS = %w{
+  appliances
+  breads_and_pastries
+  cars
+  common
+  condiments
+  djia
+  famous_movies
+  flowers
+  fruits
+  greek_gods
+  herbs_n_spices
+  industries
+  isms
+  menu_items
+  monsters
+  objects
+  occupations
+  packaging
+  paints
+  rooms
+  sculpture-materials
+  setting
+  tv_shows
+  vegetables
+  venues
+}
+
+STRUCTURES = [
+  {adj: 2, noun: 1},
+  {adj: 1, noun: 2},
+]
+
+INTRO_PHRASES = [
+  "Today I'm serving ",
+  "Today, I'm serving up some ",
+  "I am serving up some ",
+  "Today I'm serving some ",
+  "Today I'm serving you some "
+]
+
+CLOSING_PHRASES = [
+  " realness.",
+  " realness.",
+  " realness.",
+  " eleganza."
+]
+
+ADJECTIVE_FREQUENCIES = [1, 1, 1, 1, 2]
+
+sentence = "." * 141
+
+while sentence.length > 140
+  noun_file = SUBJECTS.sample
+
+  noun = open("#{noun_file}.txt").readlines.sample
+
+  adjectives = [open('selected_adjectives.txt').readlines.sample(ADJECTIVE_FREQUENCIES.sample)].flatten
+
+  phrase = adjectives.join(', ') + " " + noun
+
+  sentence = (INTRO_PHRASES.sample + phrase + CLOSING_PHRASES.sample).gsub("\n", '')
+end
+
+puts sentence
